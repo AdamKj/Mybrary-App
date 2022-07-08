180155da22872f0202779e9964b551b869a39b6f
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-07-08 14:50:46 +0200

    Initial Author routes

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const indexRouter = require("./routes/index");
-const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+
+const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
+
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("DB connected"));
 
@@ -18,7 +22,10 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 5000);
